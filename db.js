e37132f16d395d3c33217b6049b5f884990306d4
varsha <shakya.var03@gmail.com>
2024-06-13 04:16:24 -0700

    middle ware implements!!!

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-//const mongoURL = 'mongodb://localhost:27017/hotel'
+
 require('dotenv').config();
-//const mongoURL = 'mongodb+srv://shakyavar03:Varhsa123@cluster0.asybnvd.mongodb.net/'
+
 
 const mongoURL = process.env.DB_URL
 console.log(`Mongo URL: ${mongoURL}`);
@@ -9,6 +9,7 @@ mongoose.connect(mongoURL,{
   useNewUrlParser:true,
   useUnifiedTopology:true
 })
+
 
 const db = mongoose.connection;
 db.on('connected',()=>{
@@ -20,5 +21,6 @@ db.on('error',(err)=>{
 db.on('disconnected',()=>{
   console.log("mongodb disconnected")
 })
+
 
 module.exports = db;
--- server.js
+++ server.js
@@ -3,20 +3,37 @@ const app = express();
 const db = require("./db");
 require('dotenv').config();
 const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
 
+// Middleware to log requests with a timestamp
+const logRequest = (req, res, next) => {
+  const timestamp = new Date().toLocaleString();
+  console.log(`(${timestamp}) Request Made to: ${req.originalUrl}`);
+  next();
+}
+
+// Print to console immediately when the server starts
+console.log("middle ware printed");
+app.use(logRequest); // Register the logRequest middleware
+
+// Print to console immediately when the server starts
+console.log("body parser printed");
+app.use(bodyParser.json()); // Register the bodyParser.json() middleware
+
+// Define the root route
 app.get("/", function (req, res) {
   res.send("welcome to my hotel how can help you");
 });
 
+// Define and use personRoutes
 const personRoutes = require("./routes/personRoutes");
 app.use("/person", personRoutes);
+
+// Define and use menuRoutes
 const menuRoutes = require("./routes/MenuRoutes");
 app.use("/menu", menuRoutes);
 
-
-
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
-  console.log("server is running on port 3000");
+  console.log(`server is running on port ${PORT}`);
 });
